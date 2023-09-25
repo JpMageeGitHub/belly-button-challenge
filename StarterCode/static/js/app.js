e0f5ca10fe553ddb5e7afa5881fe77efb2d1aabb
JpMageeGitHub <131611035+JpMageeGitHub@users.noreply.github.com>
2023-09-25 12:41:00 -0400

    Helped!
    With help from classmate Matt, discovered where my dropdown went wrong, rebuilt code.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,49 +6,100 @@ query_url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/
 
 d3.json(query_url).then(function(data) {
     console.log(data);
-});
-let names = data.names;
-
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
-function createBarGraph(data) {
-
-    var index = []
-    var labels = []
-    var values = []
-
-    for (n in data.samples[0].otu_ids.slice(0,10)) {
-
-        index.push(n);
-        labels.push(`OTU ${data.samples[0].otu_ids[n]}`);
-        values.push(data.samples[0].sample_values[n]);
-
+  
+    // Populate the Dropdown menu
+    const names = data.names;
+    names.forEach(name => {
+      const option = d3.select("#selDataset").append("option");
+      option.text(name);
+      option.attr("value", name);
+    });
+  
+    const metadata = data.metadata;
+  
+    function makeMeta(sample) {
+      // Clear any gross existing metadata
+      d3.select("#sample-metadata").html("");
+  
+      const panel = d3.select("#sample-metadata");
+      panel.html("");
+      Object.entries(sample).forEach(([key, value]) => {
+        panel.append("h6").text(`${key}: ${value}`);
+      });
     }
-
-    var trace = {
-        x : values.reverse(),
-        y: labels.reverse(),
-        type : 'bar',
+  
+    
+  
+    // Extract needed samples
+    const samples = data.samples;
+  
+    // Function to Create Chart using Plotly
+    function makeChart(sample) {
+      // Prepare our Data
+      const { otu_ids, sample_values, otu_labels } = sample;
+      const sampleData = otu_ids.map((otu_id, index) => {
+        return {
+          otu_id,
+          sample_value: sample_values[index],
+          otu_label: otu_labels[index]
+        };
+      });
+  
+  
+      // Sort and Filter Top 10 Samples
+      const sortedData = sampleData.sort((a, b) => b.sample_value - a.sample_value).slice(0, 10);
+      sortedData.reverse();
+      
+      // Prepare data for Plotly bar chart
+      const barTrace = {
+        x: sortedData.map(d => d.sample_value),
+        y: sortedData.map(d => ` OTU ${d.otu_id} `),
+        text: sortedData.map(d => d.otu_label),
+        type: "bar",
         orientation: 'h'
-    }
-
-    var data = [trace];
-
-    var layout = {
-        title: "Top 10 OTUs by subject ID",
+      };
+  
+      // Layout for Plotly bar chart
+      const barLayout = {
+        title: "Top 10 OTUs",
         xaxis: { title: "Sample Values" },
-        yaxis: { title: "OTU IDs" }
-    };
-
-    Plotly.newPlot("bar", data, layout);
-};
-
-
-var dropdown = d3.select("#selDataset");
-
-dropdown.on("change", function() {
-    var selectedValue = d3.select(this).property("value");
-    createBarGraph(data[selectedValue]);
-});
-
-
-
+        yaxis: { title: "OTU ID" }
+      };
+  
+      // Prepare data for Plotly bubble chart
+      const bubbleTrace = {
+        x: sampleData.map(d => d.otu_id),
+        y: sampleData.map(d => d.sample_value),
+        text: sampleData.map(d => d.otu_label),
+        mode: "markers",
+        marker: {
+          size: sampleData.map(d => d.sample_value),
+          color: sampleData.map(d => d.otu_id)
+        }
+      };
+  
+      // Layout for Plotly bubble chart
+      const bubbleLayout = {
+        title: "OTU ID",
+        xaxis: { title: "OTU ID" },
+        yaxis: { title: "Sample Values" }
+      };
+  
+      // Create Plotly chart
+      Plotly.newPlot("bar", [barTrace], barLayout);
+      Plotly.newPlot("bubble", [bubbleTrace], bubbleLayout);
+    }
+  
+    // Create Chart with First Sample
+    makeChart(samples[0]);
+    makeMeta(metadata[0]);
+  
+    // Update Plot on Dropdown Change
+    d3.select("#selDataset").on("change", function() {
+      const selectedID = this.value;
+      const selectedSample = samples.find(sample => sample.id === selectedID);
+      const selectedMetadata = metadata.find(sample => sample.id === parseInt(selectedID));
+      makeChart(selectedSample);
+      makeMeta(selectedMetadata);
+    });
+  });
